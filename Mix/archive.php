247887f1f582aa8d4438a3764ae62d2bb1a68c10
nek0peko <sakura993625307@gmail.com>
2022-11-11 01:11:49 +0800

    feat: 在文章分类页面显示分类描述，修改部分文字描述和样式

--- Mix/archive.php
+++ Mix/archive.php
@@ -16,50 +16,42 @@ $this->need('component/headnav.php');
 
 <div id="main_load">
     <main class="is-article" id="article-wrap">
-
         <?php
         if ($this->options->sideBarStyle == 2) {
             $this->need('component/sidebar.php');
         }
         ?>
-
         <section class="post-title">
             <h1>
-                <div>
-                    <div class="texty mask-bottom" style="opacity: 1;"><span class=""
-                                                                             style="opacity: 1; transform: translate(0px, 0%);"><?php $this->archiveTitle(array(
-                                'category' => _t('分类 %s 下的文章'),
-                                'search' => _t('包含关键字 %s 的文章'),
-                                'tag' => _t('%s 下的文章'),
-                                'author' => _t('%s 发布的文章')
-                            ), '', ' - '); ?></span></div>
+                <div class="texty mask-bottom" style="opacity: 1;">
+                        <span style="opacity: 1; transform: translate(0px, 0%);"><?php $this->archiveTitle(array(
+                                'category' => _t('- %s'),
+                                'search' => _t('- 包含关键字“%s”的文章'),
+                                'tag' => _t('- <span style="font-weight: bolder">%s</span> 标签下的文章'),
+                                'author' => _t('- %s 发布的文章')
+                            ), '', ' - '); ?></>
                 </div>
             </h1>
             <h2>
-                <div>
-                    <div class="texty mask-bottom" style="opacity: 1;"><span class=""
-                                                                             style="opacity: 1; transform: translate(0px, 0%);">文章人，加油！</span>
-                    </div>
+                <div class="texty mask-bottom" style="opacity: 1;">
+                    <span style="opacity: 1; transform: translate(0px, 0%);"><?php
+                        if ($this->is('category')): echo $this->getDescription(); endif; ?>
+                    </span>
+                </div>
             </h2>
         </section>
-        <div>
-            <article class="post-content paul-note" style="opacity: 1;">
-                <article class="post-content paul-note article-list">
-                    <ul>
-                        <div>
-                            <?php while ($this->next()) : ?>
-                                <li class=""
-                                    style="opacity: 1; transform: translate(0px, 0px);animation: <?php $this->options->LinksAction(); ?>; ">
-                                    <a
-                                            href="<?php $this->permalink() ?>"
-                                            rel="noopener"><?php $this->title() ?></a><span
-                                            class="meta"><?php $this->date(); ?></span></li>
-                            <?php endwhile; ?>
-                        </div>
-                    </ul>
-                </article>
+        <article class="post-content paul-note" style="opacity: 1;">
+            <article class="post-content paul-note article-list">
+                <ul>
+                    <?php while ($this->next()) : ?>
+                        <li style="opacity: 1; transform: translate(0px, 0px);
+                                animation: <?php $this->options->LinksAction(); ?>; ">
+                            <a href="<?php $this->permalink() ?>" rel="noopener"><?php $this->title() ?></a>
+                            <span class="meta"><?php $this->date(); ?></span></li>
+                    <?php endwhile; ?>
+                </ul>
             </article>
-        </div>
+        </article>
     </main>
 </div>
 
