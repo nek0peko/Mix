3345802e2621f00f392ecea0201132cbaf303885
nek0peko <sakura993625307@gmail.com>
2022-11-10 19:08:43 +0800

    update: 修改图片名称

--- Mix/header.php
+++ Mix/header.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="<?php echo $GLOBALS['assetURL'] ?>css/style1.css" data-n-g="">
 
     <link rel="shortcut icon" href="<?php echo $GLOBALS['assetURL'] ?>img/favicon.ico" type="image/x-icon"/>
-    <link rel="apple-touch-icon" href="<?php echo $GLOBALS['assetURL'] ?>img/favicon2.ico" />
+    <link rel="apple-touch-icon" href="<?php echo $GLOBALS['assetURL'] ?>img/favicon.png"/>
 
     <meta itemprop="image" content="<?php $this->options->HeaderPhoto(); ?>"/>
     <!--<link href="<?php echo $GLOBALS['assetURL'] ?>kico.css" rel="stylesheet" type="text/css">-->
