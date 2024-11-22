100d25282ef8701cc051b3d659cd143d3d9cd959
nek0peko <sakura993625307@gmail.com>
2024-11-22 10:16:57 +0800

    Update: 压缩js，降低响应时间

--- Mix/header.php
+++ Mix/header.php
@@ -26,7 +26,7 @@
     <script src="<?php echo $GLOBALS['assetURL'] ?>js/local/kico.min.js"></script>
     <!--    <script src="https://cdn.jsdelivr.net/gh/Dreamer-Paul/Kico-Style/kico.min.js"></script>-->
     <script src="<?php echo $GLOBALS['assetURL'] ?>js/pre.js"></script>
-    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <style><?php $this->options->CSS(); ?></style>
     <?php if ($this->options->BackGroundImage): ?>
         <style>
